ab9a8a646caca88fbe7ba2b688171d3a5409d8c0
vundelayamini <vundelayamini@gmail.com>
2021-06-03 18:15:16 +0530

    [yamini]Add:Create connection with sql server

---
+++ AddressBookAdo.net/AddressBookRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookAdo.net
+{
+    class AddressBookRepo
+    {
+        public static String connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        public void CheckConnection()
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    connection.Open();
+                    Console.WriteLine("connection open");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                connection.Close();
+                Console.WriteLine("connection close");
+            }
+        }
+    }
+}
+
+    
---
+++ AddressBookAdo.net/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBookAdo.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome message to Addressbook ADO.Net");
+            AddressBookRepo check = new AddressBookRepo();
+            check.CheckConnection();
+        }
+    }
+}
